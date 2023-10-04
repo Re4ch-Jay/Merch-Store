3445312cb998d4b938791d675cf529730251cad9
unknown <panhareach.phat@student.cadt.edu.kh>
2023-10-04 20:14:13 +0700

    modal on add to cart

--- resources/js/Pages/Product/partials/ProductCard.jsx
+++ resources/js/Pages/Product/partials/ProductCard.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import Modal from '@/Components/Modal';
+import React, { useState } from 'react'
 
 export default function ProductCard({
     product, 
     addToCart, 
 }) {
+    const [message, setMessage] = useState(false)
+
   return (
-    
+    <>
     <div className="w-full max-w-sm bg-white border rounded-lg shadow border-gray-700">
         <a href="#">
             <img className="p-8 rounded-t-lg" src="https://m.media-amazon.com/images/I/7177d0aFt6L._AC_UY1100_.jpg" alt="product image" />
@@ -20,10 +23,16 @@ export default function ProductCard({
 
             <div className="flex items-center justify-between">
                 <span className="text-3xl font-bold text-gray-90">${ product.price }</span>
-                <button onClick={() => addToCart(product)} className="bg-blue-700 text-white hover:bg-blue-800 focus:ring-4 focus:outline-none  font-medium rounded-lg text-sm px-5 py-2.5 text-center focus:ring-blue-800">Add to cart</button>
+                <button onClick={() => {addToCart(product); setMessage(true)}} className="bg-blue-700 text-white hover:bg-blue-800 focus:ring-4 focus:outline-none  font-medium rounded-lg text-sm px-5 py-2.5 text-center focus:ring-blue-800">Add to cart</button>
             </div>
         </div>
     </div>
+    <Modal show={message} onClose={() => setMessage(false)}>
+        <h2 className="text-center text-4xl font-medium text-gray-900">
+            Successfully added to cart
+        </h2>
 
+    </Modal>
+    </>
   )
 }
