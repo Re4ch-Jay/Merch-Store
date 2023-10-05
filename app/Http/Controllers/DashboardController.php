f2d5b958643d03ae661cc5ab35736dba82073f32
Phat Panhareach <111555521+Re4ch-Jay@users.noreply.github.com>
2023-10-05 12:20:29 +0700

    Dashboard search

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,7 +10,10 @@ use Inertia\Inertia;
 class DashboardController extends Controller
 {
     public function index() {
-        $products = Product::query()->with('category')->latest()->get();
-        return Inertia::render('Dashboard', ['products' => $products]);
+        $products = Product::query()->filter(request(['search']))->with('category')->latest()->get();
+        return Inertia::render('Dashboard', [
+            'products' => $products,
+            'filters' => request()->only(['search'])
+        ]);
     }
 }
--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,8 +1,13 @@
 import Table from '@/Components/Table';
+import TextInput from '@/Components/TextInput';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import useSearch from '@/hooks/useSearch';
 import { Head, Link } from '@inertiajs/react';
 
-export default function Dashboard({ auth, products }) {
+export default function Dashboard({ auth, products, filters }) {
+    
+    const {search, handleSearch} = useSearch(filters);
+
     return (
         <AuthenticatedLayout
             user={auth.user}
@@ -12,6 +17,14 @@ export default function Dashboard({ auth, products }) {
            
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <TextInput
+                        name="search"
+                        value={search}
+                        isFocused={true}
+                        placeholder="Search..."
+                        onChange={(e) => handleSearch(e.target.value)}
+                        className="my-4 block w-full"
+                    />
                     <Link href={route('products.create')}>Upload new product</Link>
                     <Table products={products}/>
                 </div>
